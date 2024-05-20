b6d69c0ddac593e44ffee14c901c6f22f45ab9b6
mathesis <53684759+hermeneuta@users.noreply.github.com>
2024-05-20 14:28:05 +0200

    aktualizacja tworcow

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,7 @@
 import "./About.css";
 import React from "react";
 import judyta from "../assets/judyta.jpg";
+import aneta from "../assets/aneta.jpg";
 import { useNavigate } from "react-router-dom";
 import Footnote from "../Footnote/Footnote";
 
@@ -36,46 +37,85 @@ function About() {
       >
         Twórcy
       </h1>
-      <h2 className="margin:10">mgr. Judyta Kownacka</h2>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          gap: "20px",
-        }}
-      >
+      <div style={{ marginBottom: "30px" }}>
+        <h2 className="margin:10">dr n. farm. Aneta Wrzyszcz</h2>
         <div
           style={{
             display: "flex",
-            flexDirection: "column",
+            flexDirection: "row",
+            gap: "20px",
           }}
         >
-          <img
-            src={judyta}
-            alt="Judyta Kownacka"
-            style={{ width: "200px", height: "200px" }}
-          />
-          <div style={{ fontSize: "12px", marginLeft: "auto" }}>
-            fot. Anna Kobierna
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <img
+              src={aneta}
+              alt="Aneta Wrzyszcz"
+              style={{ width: "200px", height: "200px" }}
+            />
+          </div>
+          <div>
+            <p style={{ fontWeight: "bold", justifyContent: "flex" }}>
+              Pomysłodawczyni i konsultacja naukowa
+            </p>
+            <p>
+              Absolwentka Akademii Medycznej im. Piastów Śląskich we Wrocławiu
+              na kierunku Analityka Medyczna. Specjalista analityki klinicznej,
+              diagnostyki laboratoryjnej oraz medycznej hematologii
+              laboratoryjnej.
+            </p>
+            <p>
+              <span style={{ fontWeight: "bold" }}>kontakt: </span>
+              <a href="mailto:aneta.wrzyszcz@dcopih.pl">
+                aneta.wrzyszcz@dcopih.pl
+              </a>
+            </p>
           </div>
         </div>
-        <div>
-          <p style={{ fontWeight: "bold", justifyContent: "flex" }}>
-            Koordynatorka projektu
-          </p>
-          <p>
-            Pracuje w Zakładzie Diagnostyki Laboratoryjnej Dolnośląskiego
-            Centrum Onkologii, Pulmonologii i Hematologii, na stanowisku
-            młodszego asystenta jako diagnosta laboratoryjny. Absolwentka
-            Uniwersytetu Medycznego im. Piastów Śląskich we Wrocławiu na
-            kierunku Analityka Medyczna.
-          </p>
-          <p>
-            <span style={{ fontWeight: "bold" }}>kontakt: </span>
-            <a href="mailto:judyta.kownacka@dcopih.pl">
-              judyta.kownacka@dcopih.pl
-            </a>
-          </p>
+      </div>
+      <div>
+        <h2 className="margin:10">mgr. Judyta Kownacka</h2>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "20px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <img
+              src={judyta}
+              alt="Judyta Kownacka"
+              style={{ width: "200px", height: "200px" }}
+            />
+            <div style={{ fontSize: "12px", marginLeft: "auto" }}>
+              fot. Anna Kobierna
+            </div>
+          </div>
+          <div>
+            <p style={{ fontWeight: "bold", justifyContent: "flex" }}>
+              Koordynatorka projektu
+            </p>
+            <p>
+              Absolwentka Uniwersytetu Medycznego im. Piastów Śląskich we
+              Wrocławiu na kierunku Analityka Medyczna.
+            </p>
+            <p>
+              <span style={{ fontWeight: "bold" }}>kontakt: </span>
+              <a href="mailto:judyta.kownacka@dcopih.pl">
+                judyta.kownacka@dcopih.pl
+              </a>
+            </p>
+          </div>
         </div>
       </div>
       <Footnote />
